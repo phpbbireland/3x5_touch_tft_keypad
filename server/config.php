8fc36b94d4ce64d86f1daed6e68d0bc2d64b5f4e
Michael O'Toole <phpbbireland@gmail.com>
2024-12-30 18:06:43 +0000

    Add files via upload

---
+++ server/config.php
@@ -0,0 +1,11 @@
+<?php
+
+	$database = "echokb";
+	$hostname = "localhost";
+	$username = "root";
+	$password = "";
+
+
+	@define('DEBUG', true);
+	@define ('STRIP', true);
+?>
---
+++ server/install.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+*
+* @package ESP8266 Web Server
+* @version $Id$
+* @author  Michael O'Toole - aka michaelo
+* @begin   Saturday, Jan 22, 2015
+* @copyright (c) 2015 phpbbireland
+* @home    http://www.phpbbireland.com
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+*/
+
+define('IN_CODE', true);
+
+$root_path = (defined('ROOT_PATH')) ? ROOT_PATH : './';
+$phpEx = substr(strrchr(__FILE__, '.'), 1);
+
+include($root_path . 'common.' . $phpEx);
+include($root_path . 'includes/functions_main.' . $phpEx);
+include($root_path . 'includes/functions.' . $phpEx);
+
+include('partial/header.html');
+include('partial/left_blocks.html');
+include('partial/section_install.html');
+include('partial/footer.html');
